a2b32b3d97ea1a0a9035f6ee6e81e39082691d87
Siddhant Jajoo <siddhant@boulderai.com>
2021-02-09 21:19:32 +0000

    lensdriver updates

--- lensdriver/pull-docker.sh
+++ lensdriver/pull-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+
 pushd `dirname $0`
 source container.sh
 cat /data/keys/external-bai-*.json | docker login -u _json_key --password-stdin https://gcr.io
--- lensdriver/run-docker.sh
+++ lensdriver/run-docker.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-set -e
 cd `dirname $0`
 mkdir -p /data
 source container.sh
-docker run --net=host --privileged --runtime nvidia --rm --ipc=host -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v /tmp/argus_socket:/tmp/argus_socket -v /dev/video0:/dev/video0 -v /dev/i2c-2:/dev/i2c-2 -v /dev/i2c-6:/dev/i2c-6 -v /data:/data --cap-add SYS_PTRACE -e DISPLAY=$DISPLAY -it $container
+
+docker run \
+    -v /data/production:/data/production:ro \
+    -v /etc/bai-production:/etc/bai-production:ro \
+    -v /etc/bai/persist:/etc/bai/persist \
+    --privileged \
+    --runtime nvidia \
+    --rm \
+    --ipc=host \
+    --cap-add SYS_PTRACE \
+    -e DISPLAY=$DISPLAY \
+    -it ${container}
+
